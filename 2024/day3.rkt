1b4ea23ecb6e93cf843a4ace25c913e2cd0f54f7
Peter Samarin <peter.samarin@code-intelligence.com>
2024-12-03 08:26:26 +0100

    day3 part 2

--- 2024/day3.rkt
+++ 2024/day3.rkt
@@ -10,3 +10,20 @@
 (apply + (map (lambda (a) (apply * a)) data))
 
 
+;; part 2
+(define (interpret-mul str)
+  (map string->number (string-split (substring str 4 (- (string-length str) 1)) ",")))
+
+(define data2
+  (let loop ([instructions (regexp-match* #px"mul\\([[:digit:]]+,[[:digit:]]+\\)|do\\(\\)|don't\\(\\)" input)]
+             [valid? #t]
+             [result '()])
+    (cond
+      [(empty? instructions) (apply + (map (lambda (a) (apply * a)) result))]
+      [else
+       (define instr (car instructions))
+       (cond
+         [(string=? "do()" instr) (loop (cdr instructions) #t result)]
+         [(string=? "don't()" instr) (loop (cdr instructions) #f result)]
+         [valid? (loop (cdr instructions) valid? (cons (interpret-mul instr) result))]
+         [else (loop (cdr instructions) valid? result)])])))
