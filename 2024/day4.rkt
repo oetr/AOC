e79886209b25ede2677a39b42aeddc6a108fc86b
Peter Samarin <peter.samarin@code-intelligence.com>
2024-12-04 09:17:08 +0100

    day 4 part 1

---
+++ 2024/day4.rkt
@@ -0,0 +1,82 @@
+#lang racket
+
+
+(define input (file->lines "inputs/day4.txt"))
+
+;; part 1, really not proud about this :) 
+;; convert top list to vector
+(define data
+  (for/vector ([s input])
+    s))
+
+(define (eq-XMAS? d)
+  (and (string? d) (string=? d "XMAS")))
+
+(define XMASLEN (string-length "XMAS"))
+
+
+;; search for MAS
+(define (find-all-XMAS-ses data)
+  (define result 0)
+  (for ([line data]
+        [line-i (sequence-length data)])
+    (for ([char line]
+          [i (string-length line)])
+      
+      (define right (when (> (string-length line) (+ i XMASLEN -1))
+                      (substring line i (+ i XMASLEN))))
+      (define left (when (>= i (- XMASLEN 1))
+                     (list->string (for/list ([j (range i (- i XMASLEN) -1)])
+                                     (string-ref line j)))))
+      (define down (when (> (sequence-length data) (+ line-i XMASLEN -1))
+                     (list->string (for/list ([j (range line-i (+ line-i XMASLEN))])
+                                     (string-ref (vector-ref data j) i)))))
+      (define up (when (>= line-i (- XMASLEN 1))
+                   (list->string (for/list ([j (range line-i (- line-i XMASLEN) -1)])
+                                   (string-ref (vector-ref data j) i)))))
+      (define up-left (when (and (>= line-i (- XMASLEN 1))
+                                 (>= i (- XMASLEN 1)))
+                        (list->string (for/list ([x (range i (- i XMASLEN) -1)]
+                                                 [y (range line-i (- line-i XMASLEN) -1)])
+                                        (string-ref (vector-ref data y) x)))))
+      (define up-right (when (and (>= line-i (- XMASLEN 1))
+                                  (> (string-length line) (+ i XMASLEN -1)))
+                         (list->string (for/list ([x (range i (+ i XMASLEN))]
+                                                  [y (range line-i (- line-i XMASLEN) -1)])
+                                         (string-ref (vector-ref data y) x)))))
+      (define down-left (when (and (> (sequence-length data) (+ line-i XMASLEN -1))
+                                   (>= i (- XMASLEN 1)))
+                          (list->string (for/list ([x (range i (- i XMASLEN) -1)]
+                                                   [y (range line-i (+ line-i XMASLEN))])
+                                          (string-ref (vector-ref data y) x)))))
+      (define down-right (when (and (> (sequence-length data) (+ line-i XMASLEN -1))
+                                    (> (string-length line) (+ i XMASLEN -1)))
+                          (list->string (for/list ([x (range i (+ i XMASLEN))]
+                                                   [y (range line-i (+ line-i XMASLEN))])
+                                          (string-ref (vector-ref data y) x)))))
+      
+      (when (eq-XMAS? right)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? left)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? down)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? up)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? up-left)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? up-right)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? down-left)
+        (set! result (+ result 1)))
+      (when (eq-XMAS? down-right)
+        (set! result (+ result 1)))
+      ))
+  result)
+
+
+(find-all-XMAS-ses data)
+
+
+;; part 2
+
