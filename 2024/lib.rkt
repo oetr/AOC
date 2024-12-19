2bc096b3d711d4c13c53d0aff67159ec62e274ff
Peter Samarin <peter.samarin@code-intelligence.com>
2024-12-19 23:37:55 +0100

    improve the memoize macro

--- 2024/lib.rkt
+++ 2024/lib.rkt
@@ -6,8 +6,6 @@
 
 ;; define/memoize and memoize are taken from https://github.com/kstrafe/memo
 ;; and modified here. Their original licence is LGPL.
-
-
 (define-syntax-parser define/memoize
   ([_ (name:id formals:id ...+)
       (~optional (~seq #:hash hsh:expr) #:defaults [(hsh #'make-hash)])
@@ -16,18 +14,17 @@
 
 ;; only memoizes the first parameter
 (define-syntax-parser memoize-first
-  ([_ (param:id ...)
+  ([_ (param:id not-memoized-params ...)
       (~optional (~seq #:hash hsh:expr) #:defaults [(hsh #'make-hash)])
       body:expr ...+]
    #'(let ([cache (hsh)]
            [unique-token (gensym 'memoize-)])
        (case-lambda
          [() cache]
-         [(param ...)
-          (let ([data (list param ...)])
-            (let ([result (hash-ref cache (car data) unique-token)])
+         [(param not-memoized-params ...)
+            (let ([result (hash-ref cache param unique-token)])
               (if (and (symbol? result) (symbol=? unique-token result))
                   (let ([value ((lambda () body ...))])
-                    (hash-set! cache (car data) value)
+                    (hash-set! cache param value)
                     value)
-                  result)))]))))
+                  result))]))))
