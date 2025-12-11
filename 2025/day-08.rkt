2ac26fe70b36664efa008c19c2f3126967961693
Peter Samarin <peter.samarin@code-intelligence.com>
2025-12-11 23:22:30 +0100

    day 8, original, salty

--- 2025/day-08.rkt
+++ 2025/day-08.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require advent-of-code
-          data/union-find)
+         data/union-find)
 
 (define test-input "162,817,812
 57,618,57
@@ -32,63 +32,66 @@
       (string->number str-num))))
 
 (define (distance v1 v2)
-  (for/sum ([c1 v1]
-            [c2 v2])
-    (sqr (- c2 c1))))
+  (sqrt (for/sum ([c1 v1]
+                  [c2 v2])
+          (sqr (- c2 c1)))))
 
-(define (best-candidates circuits todo)
-  ;;(printf "-----------------------~a~n" connected)
-  (for*/fold ([best1 -1]
-              [best2 -1]
-              [min-distance (expt 2 64)]
-              [best-v1 empty]
-              [best-v2 empty]
-              #:result (begin
-                         (printf "~a-~a   ~a~n" best1 best2 min-distance)
-                         (values best1 best2 best-v1 best-v2)))
-             ([(v1 i) (in-hash todo)]
-              [(v2 j) (in-hash todo)]
-              #:unless (= i j)
-              #:unless (uf-same-set? (vector-ref circuits i)
-                                     (vector-ref circuits j))
-              )
-    (define dist (distance v1 v2))
-    (printf "~a - ~a: [~a]  ~a, ~a   ~a       ~a - ~a~n" i j dist best1 best2 min-distance v1 v2)
-    (if (< dist min-distance)
-        (values i j dist v1 v2)
-        (values best1 best2 min-distance best-v1 best-v2))))
+(struct distance-entry (id1 id2 distance) #:transparent)
 
-(define (part1 input iterations)
-  (define data (parse-input input))
-  (define todo (for/hash ([(d i) (in-indexed data)])
-                 (values d i)))
-  (define len (vector-length data))
+(define (get-all-distances points)
+  (sort
+   (for*/list ([(p1 i) (in-indexed points)]
+               [j (in-range (+ i 1) (vector-length points))])
+     (define p2 (vector-ref points j))
+     (distance-entry i j (distance p1 p2)))
+   (lambda (a b)
+     (< (distance-entry-distance a)
+        (distance-entry-distance b)))))
+
+(define (part1 input n (part2? #f))
+  (define points (parse-input input))
+  (define len (vector-length points))
+  (define distances (get-all-distances points))
   (define circuits
-    (for/vector ([i (vector-length data)])
+    (for/vector ([i (vector-length points)])
       (uf-new i)))
 
-  (let loop ([n 0]
-             [todo todo])
-    (printf "~a~n" n)
-    (cond
-      [(or (= n iterations) (zero? (hash-count todo)))
-       (apply * (map length (take (sort (group-by identity
-                              (for/list ([circuit circuits])
-                                (uf-find circuit)))
-                   (lambda (a b) (> (length a) (length b))))
-             3)))]
-      [else
-       (define-values (best1 best2 v1 v2) (best-candidates circuits todo))
-       (uf-union! (vector-ref circuits best1)
-                  (vector-ref circuits best2))
-       (printf "~a~n"
-               (take (sort (group-by identity
-                              (for/list ([circuit circuits])
-                                (uf-find circuit)))
-                   (lambda (a b) (> (length a) (length b))))
-             3))
-       (loop (+ n 1) (hash-remove (hash-remove todo v1) v2))])))
+  (define part2
+    (let loop ([distances distances]
+               [connected (set)]
+               [n n])
+      (define distance (car distances))
+      (define id1 (distance-entry-id1 distance))
+      (define id2 (distance-entry-id2 distance))
+      (define dist (distance-entry-distance distance))
+      (define circuit1 (vector-ref circuits id1))
+      (define circuit2 (vector-ref circuits id2))
+      (cond
+        [(and (not part2?) (= n 1)) 'DONE]
+        [(= len (set-count connected)) ]
+        [(uf-same-set? circuit1 circuit2) ;; skipping, remove sub1 in the test-input
+         (loop (cdr distances) connected (sub1 n))]
+        [else
+         (define new-set (set-add (set-add connected id1) id2))
+         (if (= len (set-count new-set))
+             (* (vector-ref (vector-ref points id1) 0)
+                (vector-ref (vector-ref points id2) 0))
+             (begin
+               (uf-union! circuit1 circuit2)
+               (loop (cdr distances) new-set (sub1 n))))])))
 
+  (if part2?
+      part2
+      (sort (group-by identity
+                      (for/list ([circuit circuits])
+                        (uf-find circuit)))
+            (lambda (a b) (> (length a) (length b))))))
 
-(part1 test-input 10)
+(define groups (part1 input 1000))
+(apply * (map length (take groups 3)))
 
+;; 4480 - wrong
+;; 123420 - Seriously WTF? In the actual task, we need to count skipped connections, but in the example,
+;; we don't. This is BS.
+
+(part1 input -1 #t)
